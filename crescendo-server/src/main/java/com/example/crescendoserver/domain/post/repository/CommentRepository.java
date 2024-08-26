cd5ddee252bc09d74fbf30a43bb0b196020fa46a
su080915 <tbr335@naver.com>
2024-08-27 04:00:47 +0900

    feat :: edit

--- crescendo-server/src/main/java/com/example/crescendoserver/domain/post/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.crescendoserver.domain.post.repository;
-
-import com.example.crescendoserver.domain.post.comment.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
-
