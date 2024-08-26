fabe7acd4f2d5d32dcbf6ececd00c2b454c601e7
su080915 <tbr335@naver.com>
2024-08-27 00:13:29 +0900

    feat :: edit

--- crescendo-server/src/main/java/com/example/crescendoserver/domain/post/dto/AddCommentRequest.java
+++ crescendo-server/src/main/java/com/example/crescendoserver/domain/post/dto/AddCommentRequest.java
@@ -20,13 +20,13 @@ public class AddCommentRequest {
     private String user;
     private int article;
 
-    public Comment toEntity() { //생성자를 사용해 객체 생성
-        return Comment.builder()
-                .comment(comment)
-                .createdDate(LocalDateTime.parse(createdDate))
-                .modifiedDate(LocalDateTime.parse(modifiedDate))
-                .user(user)
-                .article(article)
-                .build();
-    }
+//    public Comment toEntity() { //생성자를 사용해 객체 생성
+//        return Comment.builder()
+//                .comment(comment)
+//                .createdDate(LocalDateTime.parse(createdDate))
+//                .modifiedDate(LocalDateTime.parse(modifiedDate))
+//                .user(user)
+//                .article(article)
+//                .build();
+//    }
 }
