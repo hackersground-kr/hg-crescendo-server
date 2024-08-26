c94b9a1237617ce8df8c0ef03e48d96abcc8d814
jiyun0220 <145185712+jiyun0220@users.noreply.github.com>
2024-08-26 20:32:30 +0900

    feat :: MainView

--- crescendo-web/src/pages/Main.js
+++ crescendo-web/src/pages/Main.js
@@ -3,7 +3,20 @@ import "./Main.css";
 
 function Main() {
     return (
+      <div className="container">
         <div className="box"></div>
+        <img src={require("../image/var.png")} width="440px"/>
+        <input className="text"></input>
+        <h4>오늘의 추천 Todo 🔥</h4>
+        <div className="Todo"></div>
+        <h5>My Todo</h5>
+        <div className="underline"></div>
+        <div className="under">
+        <button className="plus">
+          <img src={require("../image/button.png")} className="plus"></img>
+        </button>
+        </div>
+      </div>
     );
   }
 
